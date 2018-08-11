a3dc7f3326780d48b0380b299392b410db7c8198
Sebastian Bautista <Lbautista.patarroyo@gmail.com>
2018-08-11 18:10:05 -0500

    se crea actividad de creacion de de data

---
+++ app/src/main/java/geoalert/com/co/geoalert/createAlertActivity.java
@@ -0,0 +1,13 @@
+package geoalert.com.co.geoalert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class createAlertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_alert);
+    }
+}
