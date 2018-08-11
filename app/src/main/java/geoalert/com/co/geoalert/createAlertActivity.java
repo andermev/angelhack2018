b550a6f7a5cef0d1b34457e5f6c2af295de47313
Sebastian Bautista <Lbautista.patarroyo@gmail.com>
2018-08-11 18:54:51 -0500

    se crea el objeto alert

--- app/src/main/java/geoalert/com/co/geoalert/createAlertActivity.java
+++ app/src/main/java/geoalert/com/co/geoalert/createAlertActivity.java
@@ -2,12 +2,30 @@ package geoalert.com.co.geoalert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import geoalert.com.co.geoalert.controller.CreateAlertController;
 
 public class createAlertActivity extends AppCompatActivity {
+
+    Button send = (Button)findViewById(R.id.Send);
+    Button camera = (Button)findViewById(R.id.Camera);
+    EditText editTextTitle = (EditText) findViewById(R.id.alertTitle);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_alert);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title =  editTextTitle.getText().toString();
+            }
+        });
     }
 }
---
+++ app/src/main/java/geoalert/com/co/geoalert/model/Warnings.java
@@ -0,0 +1,33 @@
+package geoalert.com.co.geoalert.model;
+
+public class Warnings {
+
+    private String name;
+    private String description;
+    private String ubication;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUbication() {
+        return ubication;
+    }
+
+    public void setUbication(String ubication) {
+        this.ubication = ubication;
+    }
+
+}
