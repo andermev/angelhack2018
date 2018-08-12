c601b9372ebc295bc06b8a25710a9b25cafeda57
Anderson Mejia <ander1298@gmail.com>
2018-08-11 20:16:49 -0500

    Login y Map

--- app/src/main/java/geoalert/com/co/geoalert/login.java
+++ app/src/main/java/geoalert/com/co/geoalert/login.java
@@ -1,13 +1,149 @@
 package geoalert.com.co.geoalert;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import geoalert.com.co.geoalert.model.User;
+import geoalert.com.co.geoalert.view.MapActivity;
 
 public class login extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener  listener;
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference ref = db.getReference("User");
+
+    private TextInputEditText tietUsuario;
+    private TextInputLayout tielUsuario;
+    private TextInputEditText tietPass;
+    private TextInputLayout tielPass;
+    private User user;
+    Map<String,Object> useData = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+        initViews();
+    }
+
+    private void initViews() {
+        tietUsuario = findViewById(R.id.tieUsuario);
+        tietPass = findViewById(R.id.tiePassword);
+    }
+
+    public void udpateUsers() {
+        listener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    useData.put("name",user.getDisplayName());
+                    useData.put("name",user.getDisplayName());
+                    useData.put("id",user.getUid());
+                    useData.put("email",user.getEmail());
+                    useData.put("photoUrl",user.getEmail());
+                    useData.put("providerId",user.getEmail());
+                    ref.child(user.getUid()).updateChildren(useData);
+                    startActivity(new Intent(login.this, MapActivity.class));
+                } else {
+                    // User is signed out
+                    Log.d("~~ :", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    private boolean validateForm(String email,String pass) {
+        if (TextUtils.isEmpty(email)) {
+            tielUsuario.setError("Faltan ingresar el correo Electronico");
+           return false;
+        } else  {
+            tietPass.setError(null);
+        }
+
+        if (TextUtils.isEmpty(pass)) {
+            tietPass.setError("Faltan ingresar la contraseña");
+            return false;
+        } else  {
+            tietPass.setError(null);
+        }
+        return false;
+    }
+
+
+    public void procesarLogin(final View view) {
+        user.setEmail(tietUsuario.getText().toString().trim());
+        user.setPass(tietPass.getText().toString().trim());
+
+        if (!validateForm(user.getEmail(),user.getPass())) {
+            return;
+        }
+
+        showProgressDialog("Estamos Validando los datos");
+        auth.signInWithEmailAndPassword(user.getEmail(),user.getPass())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = auth.getCurrentUser();
+                            Log.d("----TMM ","Succes: " + user);
+                            udpateUsers();
+                            startActivity(new Intent(login.this, MapActivity.class));
+                        } else {
+                            Log.w("---//Error: ", "signInWithEmail:failure", task.getException());
+                            Snackbar snackbar = Snackbar.make(view,"No Se pudo iniciar session",Snackbar.LENGTH_SHORT);
+                            snackbar.show();
+                        }
+
+                        if (!task.isSuccessful()) {
+                            Snackbar snackbar = Snackbar.make(view,"Correo o Contraseña incorrectos",Snackbar.LENGTH_SHORT);
+                            snackbar.show();
+                        }
+                        hideProgressDialog();
+                    }
+                });
+    }
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog(String message) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(message);
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }
--- app/src/main/java/geoalert/com/co/geoalert/model/User.java
+++ app/src/main/java/geoalert/com/co/geoalert/model/User.java
@@ -1,4 +1,81 @@
 package geoalert.com.co.geoalert.model;
 
+import android.net.Uri;
+
+import java.net.URI;
+
 public class User {
+
+    String nombre;
+    String uId;
+    String pass;
+    String sessionId;
+    String email;
+    Uri photoUrl;
+    Boolean emailVerified;
+    String providerId;
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Uri getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(Uri photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Boolean getEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(Boolean emailVerified) {
+        this.emailVerified = emailVerified;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
 }
