096641ebe7bb951504b58fc61363b7b1498e0f1d
Milvia Alvarez <milvampa1@gmail.com>
2018-08-11 20:06:06 -0500

    Add snapshot

--- app/src/main/java/geoalert/com/co/geoalert/view/WarningDetail.java
+++ app/src/main/java/geoalert/com/co/geoalert/view/WarningDetail.java
@@ -3,8 +3,12 @@ package geoalert.com.co.geoalert.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import geoalert.com.co.geoalert.R;
 
@@ -17,8 +21,17 @@ public class WarningDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warning_detail);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference("alerts");
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("snapshot: " + dataSnapshot.getValue(String.class));
+            }
 
-        //mDatabase.
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("snapshot: " + databaseError.toException());
+            }
+        });
     }
 }
