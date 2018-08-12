1c759b8f671600d9269f2f42de50f1083fc743cc
Milvia Alvarez <milvampa1@gmail.com>
2018-08-11 20:40:34 -0500

    Return warning dto

--- app/src/main/java/geoalert/com/co/geoalert/view/WarningDetail.java
+++ app/src/main/java/geoalert/com/co/geoalert/view/WarningDetail.java
@@ -1,9 +1,8 @@
 package geoalert.com.co.geoalert.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -11,6 +10,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import geoalert.com.co.geoalert.R;
+import geoalert.com.co.geoalert.model.Warning;
 
 public class WarningDetail extends AppCompatActivity {
 
@@ -25,7 +25,8 @@ public class WarningDetail extends AppCompatActivity {
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                System.out.println("snapshot: " + dataSnapshot.getValue(String.class));
+                Warning warning = dataSnapshot.getValue(Warning.class);
+                System.out.println("snapshot: " + warning.getDescription());
             }
 
             @Override
